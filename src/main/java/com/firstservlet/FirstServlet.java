de7c84ff4e4af1a72404b59faf36ba6d606c25c3
unknown <you@example.com>
2022-12-14 22:39:49 +0530

    Added : First servlet

--- src/main/java/com/firstservlet/FirstServlet.java
+++ src/main/java/com/firstservlet/FirstServlet.java
@@ -6,8 +6,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-
-
 @WebServlet("/FirstServlet")
 public class FirstServlet extends HttpServlet {
     @Override
