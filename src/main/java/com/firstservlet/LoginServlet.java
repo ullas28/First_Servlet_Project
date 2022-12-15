fb50208615414bb9eb70f3ac32b532548bfd18ec
unknown <you@example.com>
2022-12-15 19:32:35 +0530

    Added : valid name

--- src/main/java/com/firstservlet/LoginServlet.java
+++ src/main/java/com/firstservlet/LoginServlet.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @WebServlet(
         description = "Login Servlet Testing",
@@ -28,15 +30,22 @@ public class LoginServlet extends HttpServlet {
         String userID = getServletConfig().getInitParameter("user");
         String password = getServletConfig().getInitParameter("password");
 
-        if(userID.equals(user) && password.equals(pwd)) {
-            request.setAttribute("user", user);
-            request.getRequestDispatcher("LoginSuccess.jsp").forward(request, response);
-        } else {
-            RequestDispatcher rd = getServletContext().getRequestDispatcher("/index.html");
-            PrintWriter out = response.getWriter();
-            out.println("<font color=red>Either user name or password is wrong.</font>");
-            rd.include(request, response);
+        //regex for username
+        String userRegex = "^[A-Z][a-z]{2,}$";
 
+        if (Pattern.matches("^[A-Z][a-z]{2,}", user)){
+            if(userID.equals(user) && password.equals(pwd)) {
+                request.setAttribute("user", user);
+                request.getRequestDispatcher("LoginSuccess.jsp").forward(request, response);
+            } else {
+                RequestDispatcher rd = getServletContext().getRequestDispatcher("/index.html");
+                PrintWriter out = response.getWriter();
+                out.println("<font color=red>Either user name or password is wrong.</font>");
+                rd.include(request, response);
+            }
+            }else {
+            PrintWriter out = response.getWriter();
+            out.println("<font color=red>Kindly Enter Correct USER ID</font>");
         }
     }
 }
